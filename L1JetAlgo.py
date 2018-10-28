8a7e9557d4c802492b329182ff345eda93291013
Zhenbin Wu <zhenbin.wu@gmail.com>
2018-10-28 01:31:52 -0500

    Vectorization operation

--- L1JetAlgo.py
+++ L1JetAlgo.py
@@ -10,16 +10,16 @@
 
 
 import uproot
-import numpy as np
 import itertools
+import numpy as np
 import ROOT
-from pyjet import cluster
 import math
-from rootpy.plotting import Hist,Canvas
-from rootpy.plotting.style import get_style
-import ROOT
-from JetStudy import JetStudy
+
+from pyjet import cluster
 from rootpy.io import root_open
+
+## User
+from JetStudy import JetStudy
 
 IsTest=False
 
@@ -38,18 +38,37 @@ def CluMetric(x, y, k, R=0.4):
     dphi = pow(deltaPhi(x[2], y[2]), 2)
     return  minv * (deta+dphi) / R**2
 
+# CluMetric vectorization
+def CluMetricV(x, y, k, R=0.4):
+    Xpt = np.power(x[:, 0], k * 2)
+    Ypt = np.power(y[:, 0], k * 2)
+    deta = np.power(x[:, 1]-y[:, 1], 2)
+    dPhi = x[:, 2] - y[:, 2]
+    np.subtract(dPhi, 2*math.pi, out = dPhi, where= (dPhi >=math.pi))
+    np.add(dPhi, 2*math.pi,  out =dPhi , where=(dPhi < -1*math.pi))
+    np.power(dPhi, 2, out=dPhi)
+    return np.multiply(np.divide(np.add(deta, dPhi), R**2), np.minimum(Xpt, Ypt))
+
 def CludR(x, y):
     deta = pow(x[1]-y[1], 2)
     dphi = pow(deltaPhi(x[2], y[2]), 2)
     return (deta+dphi)
 
+# CluR vectorization
+def CludRV(x, y):
+    deta = np.power(x[:, 1]-y[:, 1], 2)
+    dPhi = x[:, 2] - y[:, 2]
+    np.subtract(dPhi, 2*math.pi, out = dPhi, where= (dPhi >=math.pi))
+    np.add(dPhi, 2*math.pi,  out =dPhi , where=(dPhi < -1*math.pi))
+    np.power(dPhi, 2, out=dPhi)
+    return  np.add(deta, dPhi)
+
 def GetSeparationList(idx, pars, k, R):
-    combs = list(itertools.combinations(idx, 2))
     combm = {}
-    for i in combs:
-        first = list(i)[0]
-        second = list(i)[1]
-        combm[i]  = CluMetric(list(pars[first]), list(pars[second]), k, R)
+    if len(idx) >= 2:
+        combs  = np.asarray(list(itertools.combinations(idx, 2)))
+        vecVal = CluMetricV(pars[combs[:, 0]], pars[combs[:, 1]], k, R)
+        combm  = dict(zip(tuple(map(tuple, combs)) , tuple(vecVal)))
     for i in idx:
         combm[(i, i)] = list(pars[i])[0]**(2*k)
     return combm
@@ -57,16 +76,17 @@ def GetSeparationList(idx, pars, k, R):
 
 def GetDistanceList(idx, pars, R):
     ptlist ={list(pars[x])[0] : x for x in idx}
-    if len(ptlist.keys()) == 0:
-        return {}
-    maxidx = ptlist[max(ptlist.keys())]
     combm = {}
+    if len(ptlist.keys()) == 0:
+        return combm
 
-    for i in idx:
-        if i == maxidx:
-            combm[(i, i)] = R**2
-        else:
-            combm[(maxidx, i)] = CludR(list(pars[maxidx]), list(pars[i]))
+    maxidx = ptlist[max(ptlist.keys())]
+    npars  = pars[idx]
+    maxv   = np.tile(pars[maxidx], (len(npars),1))
+    dis    = CludRV(maxv, npars)
+    maxk   = tuple([(maxidx, x) for x in idx])
+    combm  = dict(zip(maxk, dis))
+    combm[(maxidx, maxidx)] = R**2
     return combm
 
 
@@ -99,11 +119,9 @@ class ClusAK4:
             self.SepList = GetSeparationList(self.ObjList, self.pars, self.k, self.R)
         else:
             self.SepList = GetDistanceList(self.ObjList, self.pars, self.R)
-        # print("Dine ini")
 
     def GetMin(self):
         m = min(self.SepList, key=self.SepList.get)
-        # print(m)
         if m[0] == m[1]:
             self.MoveSingleToJet(m[0])
         else:
@@ -117,7 +135,6 @@ class ClusAK4:
     def MergingObjs(self, m):
         if self.merMed is "EMerge":
             newobj = EMerging(self.pars[m[0]], self.pars[m[1]])
-            # print("newobj", newobj)
         else:
             newobj = WTAMerging(self.pars[m[0]], self.pars[m[1]])
         self.pars=np.concatenate((self.pars, np.array([newobj])), axis=0)
@@ -125,11 +142,9 @@ class ClusAK4:
         newIdx =len(self.pars)-1
         self.ObjList.append(newIdx)
         self.UpdateLists()
-        # print(self.JetList)
 
     def UpdateLists(self):
         if self.cluMed  is "metric":
-            # print("i nupdatelist")
             self.SepList = GetSeparationList(self.ObjList, self.pars, self.k, self.R)
         else:
             self.SepList = GetDistanceList(self.ObjList, self.pars, self.R)
@@ -138,7 +153,6 @@ class ClusAK4:
     def Run(self):
         while len(self.ObjList) > 0:
             self.GetMin()
-        # print(self.JetList)
         return self.JetList
 
 
@@ -208,5 +222,3 @@ if __name__ == "__main__":
         for j in files.readlines():
             ifiles.append(j.strip())
     StudyInputs(ifiles, "PF", args.outputFile)
-
-
